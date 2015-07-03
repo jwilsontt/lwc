4bc588be2cf1b33002a2c17caa01e51c7bc3c4a0
jwilsontt <jwilsontt@gmail.com>
2015-07-03 07:58:42 -0400

    Created Social Sharing Page

--- src/joins/views.py
+++ src/joins/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
 # Create your views here.
 from .forms import EmailForm, JoinForm
@@ -26,20 +26,13 @@ def get_ref_id():
 	except:
 		return ref_id
 
+def share (request, ref_id):
+ 	context = {"ref_id": ref_id}
+ 	template = "share.html"
+ 	return render(request, template, context)
+
+
 def home(request):
-
-	# form = EmailForm(request.POST or None)
-	# if form.is_valid():
-	# 	email = form.cleaned_data["email"]
-	# 	new_join, created = Join.objects.get_or_create(email=email)
-	# 	if created:
-	# 		new_join_old.ref_id = get_ref_id()
-	# 		if not obj == None:
-	# 			new_join_old.friend = obj
-	# 		new_join_old.ip_address = get_ip(request)
-	# 		new_join_old.save()
-
-	#this is using model forms
 	form = JoinForm(request.POST or None)
 	if form.is_valid():
 		new_join = form.save(commit=False)
@@ -50,8 +43,7 @@ def home(request):
 			new_join_old.ip_address = get_ip(request)
 			new_join_old.save()
 		# redirect here
-		# new_join.ip_address = get_ip(request)
-		# new_join.save()
+		return HttpResponseRedirect("/%s" %(new_join_old.ref_id))
 
  	context = {"form": form}
  	template = "home.html"
